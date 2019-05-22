cf302d661c72b8be062fc46946619c9a801bb1b6
mydeadmemory <rogozik.alexander@gmail.com>
2019-05-22 19:00:52 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Александр Рогозик on 5/22/19.
+//  Copyright © 2019 Александр Рогозик. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
