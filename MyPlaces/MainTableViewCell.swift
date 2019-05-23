e9546f81b93e5c1f07fc82a4d409271b640e25e9
mydeadmemory <rogozik.alexander@gmail.com>
2019-05-23 17:22:30 +0300

    initial

---
+++ MyPlaces/MainTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Александр Рогозик on 5/22/19.
+//  Copyright © 2019 Александр Рогозик. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- MyPlaces/ViewController.swift
+++ MyPlaces/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
         // Do any additional setup after loading the view.
     }
-
-
+    var array = ["Балкан Гриль","Бочка","Вкусные истории","Дастархан","Индокитай","Классик","Шок"]
+    
 }
-
+extension ViewController : UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MainTableViewCell", for: indexPath) as? MainTableViewCell else {return UITableViewCell()}
+        cell.textLabel?.text = array[indexPath.row]
+        cell.imageView?.image = UIImage(named: array[indexPath.row])
+    
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let share = UITableViewRowAction(style: .default, title: "Share") { (action, indexPath) in
+            self.array.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+        let delete = UITableViewRowAction(style: .default, title: "Delete") { (action, indexPath) in
+            self.array.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+        return [share, delete]
+    }
+}
